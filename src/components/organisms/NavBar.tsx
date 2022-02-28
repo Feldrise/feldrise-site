ff9b99d363565caaf81959048aac15e43b47acdc
suiramdev <suiramdev.official@gmail.com>
2022-02-28 16:17:46 +0100

    :sparkles: Add NavBar

---
+++ src/components/organisms/NavBar.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import NavBarBtn from '../atoms/NavBarBtn';
+
+const NavBar = () => {
+	return (
+		<div className="px-28 py-8 flex justify-between">
+			<Link href="/" passHref={true}>
+				<Image src="/images/logo-surrounded.svg" alt="Website's logo" width={115} height={115} className="hover:cursor-pointer"/>
+			</Link>
+			<div className="flex items-center gap-8">
+				<NavBarBtn href="#">Projets</NavBarBtn>
+				<NavBarBtn href="#">À propos</NavBarBtn>
+				<NavBarBtn href="#">Contact</NavBarBtn>
+			</div>
+			<Link href="#" passHref={true}>
+				<Image src="/images/icons/letter.svg" alt="Contact button's logo" width={115} height={115} className="hover:cursor-pointer"/>
+			</Link>
+		</div>
+	);
+};
+
+export default NavBar;
