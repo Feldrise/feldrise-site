e5594cfa163cc6dae801db59d2856b55ce7abd71
suiramdev <suiramdev.official@gmail.com>
2022-03-09 17:41:41 +0100

    :lipstick: Fully designed Hero section

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,9 +5,13 @@ module.exports = {
 	],
 	theme: {
 		extend: {
+			backgroundImage: {
+				'hero': 'url("/images/hero.png")',
+			},
 			colors: {
 				'neutral': '#1F1F1F',
 				'primary': '#AA33FF',
+				'primary-dark': '#860DDC',
 				'secondary': '#FF33DE',
 			},
 		},
