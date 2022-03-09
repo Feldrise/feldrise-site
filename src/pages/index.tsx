ccb8ef64d2f21650a6d9db8c49bb18069c07b502
suiramdev <suiramdev.official@gmail.com>
2022-03-09 17:41:41 +0100

    :twisted_rightwards_arrows: Rebase feat/homepage onto dev Fixed merge conflits

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,8 @@
 import {useContext, useEffect} from 'react';
 import type {NextPage} from 'next';
+import Link from 'next/link';
+import Image from 'next/image';
+import cx from 'classnames';
 import NavBarContext from '../context/NavBarContext';
 
 const Home: NextPage = () => {
@@ -12,6 +15,26 @@ const Home: NextPage = () => {
 
 	return (
 		<div>
+			<section className="relative h-screen bg-hero">
+				<div className={cx(
+					'absolute top-1/2 left-0 -translate-y-1/2 p-12 flex flex-col gap-6',
+					'bg-white rounded-r-2xl shadow-lg shadow-black/30'
+				)}>
+					<div className="relative w-full h-[75px]">
+						<Image src="/images/logo-dark.svg" layout="fill" objectFit="contain" objectPosition="left" alt="Hero's background"/>
+					</div>
+					<div>
+						<span className="mr-6 font-bold text-4xl">Digital Agency</span>
+						<span className="italic text-2xl">basée à Rennes</span>
+					</div>
+					<Link href="/">
+						<a className={cx(
+							'w-fit px-8 py-4',
+							'rounded-lg bg-primary font-medium text-white text-2xl hover:bg-primary-dark transition-colors'
+						)}>Voir les projets</a>
+					</Link>
+				</div>
+			</section>
 		</div>
 	);
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,9 @@ module.exports = {
 	],
 	theme: {
 		extend: {
+			backgroundImage: {
+				'hero': 'url("/images/hero.png")',
+			},
 			colors: {
 				'neutral': '#1F1F1F',
 				'primary': '#AA33FF',
