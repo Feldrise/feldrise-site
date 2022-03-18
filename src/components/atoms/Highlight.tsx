7d426231eb1d806c0f9ba728912605470e972f8e
suiramdev <suiramdev.official@gmail.com>
2022-03-18 15:34:31 +0100

    :sparkles: Highlighting animation in-view

--- src/components/atoms/Highlight.tsx
+++ src/components/atoms/Highlight.tsx
@@ -1,12 +1,45 @@
-import React from 'react';
+import React, {useEffect, useRef, useState} from 'react';
+import cx from 'classnames';
 
 interface IHighlightProps {
 	children?: React.ReactNode;
 }
 
 const Highlight = (props: IHighlightProps) => {
+	const [visible, setVisible] = useState<boolean>(false);
+	const observedRef = useRef<HTMLSpanElement>(null);
+
+	/*
+	 * Callback of IntersactionObserver used to animate the highlighting
+	 * when it is displayed on view
+	 */
+	const observerCallback = (entries: IntersectionObserverEntry[]) => {
+		const [entry] = entries;
+		if (entry.isIntersecting) {
+			setVisible(true);
+		}
+	};
+
+	useEffect(() => {
+		const observer = new IntersectionObserver(observerCallback);
+		const observedEl = observedRef.current;
+		observedEl && observer.observe(observedEl);
+
+		return () => {
+			observedEl && observer.unobserve(observedEl);
+		};
+	});
+
 	return (
-		<span className="relative px-4 py-2 bg-secondary whitespace-nowrap">
+		<span
+			className={cx(
+				'relative px-4 py-2 whitespace-nowrap z-0',
+				'before:content-[""] before:absolute before:h-full before:top-0 before:left-0',
+				visible ? 'before:w-full' : 'before:w-0',
+				'before:bg-secondary before:-z-10 before:transition-all before:duration-1000'
+			)}
+			ref={observedRef}
+		>
 			{props.children}
 		</span>
 	);
