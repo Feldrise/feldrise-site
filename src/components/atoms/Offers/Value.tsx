386c0c53a4b388025dafebe456f58e50a2afba78
suiramdev <suiramdev.official@gmail.com>
2022-03-22 16:43:35 +0100

    :lipstick: Added offers sections

---
+++ src/components/atoms/Offers/Value.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faSquareCheck} from '@fortawesome/free-solid-svg-icons';
+
+interface IValueProps {
+	content: string;
+}
+
+const Value = (props: IValueProps) => {
+	return (
+		<div className="flex items-center gap-4">
+			<FontAwesomeIcon icon={faSquareCheck} className="w-[32px] h-[32px] text-primary"/>
+			<p className="p-8 bg-primary rounded-xl">
+				{props.content}
+			</p>
+		</div>
+	);
+};
+
+export default Value;
