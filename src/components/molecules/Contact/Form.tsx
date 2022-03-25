0d64c9fec8c1eea4bb3f173ce6c45ee0ded204f6
suiramdev <suiramdev.official@gmail.com>
2022-03-25 16:34:49 +0100

    :sparkles: The whole email sys. No captcha verification and templates yet

--- src/components/molecules/Contact/Form.tsx
+++ src/components/molecules/Contact/Form.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Field from '../../atoms/Contact/Field';
 import Button from '../../atoms/Button';
 import FieldGroup from './FieldGroup';
 import axios from 'axios';
 import {FieldValues, useForm} from 'react-hook-form';
+import {GoogleReCaptchaProvider, GoogleReCaptcha} from 'react-google-recaptcha-v3';
 
 export interface IContactFormData extends FieldValues {
 	fullName: string;
@@ -15,40 +16,48 @@ export interface IContactFormData extends FieldValues {
 
 const Form = () => {
 	const {register, handleSubmit} = useForm<IContactFormData>();
+	const [captchaToken, setCaptchaToken] = useState<string>();
 
 	// eslint-disable-next-line @typescript-eslint/ban-types
 	const onSubmit = async (data: IContactFormData) => {
 		await axios.post('/api/mail', {
+			captchaToken,
 			email: data.email,
-			message: `
-			${data.fullName}\n
-			${data.phone}\n\n
-			${data.message}
-			`,
+			fullName: data.fullName,
+			phone: data.phone,
+			society: data.society,
+			message: data.message,
 		});
 	};
 
+	const onCaptchaVerify = (token: string) => {
+		setCaptchaToken(token);
+	};
+
 	return (
-		<form className="w-full p-8 flex flex-col justify-center items-center gap-4" onSubmit={handleSubmit(onSubmit)}>
-			<FieldGroup>
-				<Field label="Nom et prénom" placeholder="Clément Tine" name="fullName" register={register} required/>
-				<Field label="Société" placeholder="Microsoft" name="society" register={register} required/>
-			</FieldGroup>
-			<FieldGroup>
-				<Field label="Mail" placeholder="clementine@gmail.com" name="email" register={register} required/>
-				<Field label="Téléphone" placeholder="0666708090" name="phone" register={register} required/>
-			</FieldGroup>
-			<Field
-				label="Le sujet de votre message"
-				type="rich"
-				placeholder="Bonjour, je souhaite créer une application..."
-				inputClassName="min-h-[200px]"
-				name="message"
-				register={register}
-				required
-			/>
-			<Button type="submit">Envoyer mon message</Button>
-		</form>
+		<GoogleReCaptchaProvider reCaptchaKey={process.env.NEXT_PUBLIC_CAPTCHA_SITE}>
+			<form className="w-full p-8 flex flex-col justify-center items-center gap-4" onSubmit={handleSubmit(onSubmit)}>
+				<FieldGroup>
+					<Field label="Nom et prénom" placeholder="Clément Tine" name="fullName" register={register} required/>
+					<Field label="Société" placeholder="Microsoft" name="society" register={register}/>
+				</FieldGroup>
+				<FieldGroup>
+					<Field label="Mail" placeholder="clementine@gmail.com" name="email" register={register} required/>
+					<Field label="Téléphone" placeholder="0666708090" name="phone" register={register}/>
+				</FieldGroup>
+				<Field
+					label="Le sujet de votre message"
+					type="rich"
+					placeholder="Bonjour, je souhaite créer une application..."
+					inputClassName="min-h-[200px]"
+					name="message"
+					register={register}
+					required
+				/>
+				<GoogleReCaptcha onVerify={onCaptchaVerify}/>
+				<Button type="submit">Envoyer mon message</Button>
+			</form>
+		</GoogleReCaptchaProvider>
 	);
 };
 
--- src/pages/api/mail.ts
+++ src/pages/api/mail.ts
@@ -1,33 +1,83 @@
 import type {NextApiRequest, NextApiResponse} from 'next';
-import nodemailer, {TestAccount, Transporter} from 'nodemailer';
+import nodemailer, {Transporter} from 'nodemailer';
 import SMTPTransport from 'nodemailer/lib/smtp-transport';
+// import reCaptchaMiddleware from '../../middlewares/reCaptcha';
+
+/*
+import * as fs from 'fs';
+import path from 'path';
+
+const loadTemplate = (templatePath: string, replacements?: {[key: string]: string}): string => {
+	try {
+		const content = fs.readFileSync(path.join(__dirname, templatePath), 'utf8');
+		for (const key in replacements) {
+			if (!replacements.hasOwnProperty(key)) continue; // what the hell is that
+
+			const value = replacements[key];
+			content.replaceAll(`{{${key}}}`, value);
+		}
+		return content;
+	} catch (e) {
+		console.log(e);
+		return '';
+	}
+};
+*/
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
 	if (req.method !== 'POST') return;
+	// await reCaptchaMiddleware(req, res);
 
 	try {
-		const testMail: TestAccount = await nodemailer.createTestAccount();
-
-		const transporter: Transporter = nodemailer.createTransport({
-			host: testMail.smtp.host,
-			port: testMail.smtp.port,
-			secure: testMail.smtp.secure,
+		const transport: SMTPTransport.Options = {
+			host: process.env.MAIL_HOST,
+			port: parseInt(process.env.MAIL_PORT || '435'),
+			secure: process.env.MAIL_SECURE === 'true',
 			auth: {
-				user: testMail.user,
-				pass: testMail.pass,
+				user: process.env.MAIL_USER,
+				pass: process.env.MAIL_PASS,
 			},
+		};
+		/* if (process.env.NODE_ENV !== 'production') {
+			const testMail: TestAccount = await nodemailer.createTestAccount();
+
+			transport = {
+				host: testMail.smtp.host,
+				port: testMail.smtp.port,
+				secure: testMail.smtp.secure,
+				auth: {
+					user: testMail.user,
+					pass: testMail.pass,
+				},
+			};
+		}*/
+
+		const transporter: Transporter = nodemailer.createTransport(transport);
+
+		/* const sentMail: SMTPTransport.SentMessageInfo = */ await transporter.sendMail({
+			from: process.env.MAIL_USER,
+			to: process.env.MAIL_RECEIVER,
+			replyTo: `"${req.body.fullName}" <${req.body.email}>`,
+			subject: `Demande de contacte de ${req.body.fullName}`,
+			text: `Nom Prénom: ${req.body.fullName}\nSociété: ${req.body.society || 'N/A'}
+			\nNuméro de téléphone: ${req.body.phone || 'N/A'}\n\nMessage: ${req.body.message}`,
+			/*
+			html: loadTemplate('templates/mail/contact.html', {
+				email: req.body.email,
+				fullName: req.body.fullName,
+				phone: req.body.phone,
+				message: req.body.email,
+			}),
+			*/
 		});
 
-		const sentMail: SMTPTransport.SentMessageInfo = await transporter.sendMail({
-			from: req.body.email,
-			to: 'suiramdev.official@gmail.com',
-			subject: 'Message du portfolio',
-			text: req.body.message,
-		});
-		console.log(nodemailer.getTestMessageUrl(sentMail));
+		/* if (process.env.NODE_ENV !== 'production') {
+			console.log(nodemailer.getTestMessageUrl(sentMail));
+		}*/
 
 		res.status(200);
 	} catch (e) {
+		console.log(e);
 		res.status(500).send(e);
 	}
 };
