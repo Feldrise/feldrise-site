ef9a51f07623ef454aea66bd52d6cdb6e53b548a
HamsterOh <mariusnouchet@gmail.com>
2022-03-31 19:44:02 +0200

    :lipstick: Display projects in grids

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -4,51 +4,26 @@ import Section from '../../components/organisms/Section';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import Project from '../../components/molecules/Project';
-import {Swiper, SwiperSlide} from 'swiper/react';
 import 'swiper/css';
-import {Autoplay, FreeMode} from 'swiper';
+import cx from 'classnames';
 
-const Projects: NextPage = () => {
-	return (
+const Projects: NextPage = () =>
+	(
 		<Section className="my-20">
-			<Swiper
-				modules={[FreeMode, Autoplay]}
-				direction="vertical"
-				slidesPerView={2}
-				loop={true}
-				autoplay={{
-					delay: 1500,
-					disableOnInteraction: false,
-					pauseOnMouseEnter: true,
-					reverseDirection: true,
-				}}
-				freeMode={{
-					enabled: true,
-					sticky: false,
-					momentum: true,
-				}}
-				className="h-screen overflow-hidden"
-			>
-				{[0].map((project, index) =>
-					<SwiperSlide key={index} className="grid grid-cols-1 lg:grid-cols-2 gap-4">
-						<Link href="/projects/[name]" as="/projects/pluctis" passHref>
-							<Project
-								name="Pluctis"
-								desc="Janvier 2021 - Mars 2021"
-							/>
-						</Link>
-						<Link href="/projects/[name]" as="/projects/pluctis" passHref>
-							<Project
-								name="Pluctis"
-								desc="Janvier 2021 - Mars 2021"
-								className="lg:mt-12"
-							/>
-						</Link>
-					</SwiperSlide>
-				)}
-			</Swiper>
+			<div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
+				{Array.from(new Array(10)).map((_, index) => (
+					<Link key={index} href="/projects/[name]" as="/projects/pluctis" passHref>
+						<Project
+							name="Pluctis"
+							desc="Janvier 2021 - Mars 2021"
+							className={cx(
+								index % 2 !== 0 && 'mt-20',
+							)}
+						/>
+					</Link>
+				))}
+			</div>
 		</Section>
 	);
-};
 
 export default Projects;
