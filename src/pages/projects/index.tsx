b5bf92ab95cadc9c76a9f8fc798a66e4e2a93481
Marius <71449142+suiramdev@users.noreply.github.com>
2022-04-10 22:29:02 +0200

    Fix/projects (#31)
    * :lipstick: Display projects in grids
    
    * :sparkles: Kind-of dynamic projects

--- src/pages/projects/index.tsx
+++ src/pages/projects/index.tsx
@@ -4,51 +4,83 @@ import Section from '../../components/organisms/Section';
 import 'swiper/css';
 import 'swiper/css/navigation';
 import Project from '../../components/molecules/Project';
-import {Swiper, SwiperSlide} from 'swiper/react';
 import 'swiper/css';
-import {Autoplay, FreeMode} from 'swiper';
+import cx from 'classnames';
 
-const Projects: NextPage = () => {
-	return (
+interface IProjectInfo {
+	name: string;
+	value: string;
+}
+
+interface IProjectTech {
+	name: string;
+	icon: string;
+}
+
+interface IProject {
+	label: string;
+	name: string
+	images: string[];
+	desc: string;
+	infos?: IProjectInfo[];
+	techs?: IProjectTech[];
+}
+
+export const projects: IProject[] = [
+	{
+		label: 'Pluctis',
+		name: 'pluctis',
+		images: [
+			'/images/projects/pluctis.png',
+			'/images/projects/pluctis.png',
+			'/images/projects/pluctis.png',
+		],
+		desc: 'Janvier 2021 - Mars 2021',
+		infos: [
+			{
+				name: 'Terminé',
+				value: '01 avril 2022',
+			},
+			{
+				name: 'Client',
+				value: 'Pluctis Inc.',
+			},
+			{
+				name: 'Rôle',
+				value: 'Développeur fullstack',
+			},
+			{
+				name: 'Type',
+				value: 'App mobile',
+			},
+		],
+		techs: [
+			{
+				name: 'Flutter & Dart',
+				icon: '/images/icons/flutter.svg',
+			},
+		],
+	},
+];
+
+const Projects: NextPage = () =>
+	(
 		<Section className="my-20">
-			<Swiper
-				modules={[FreeMode, Autoplay]}
-				direction="vertical"
-				slidesPerView={2}
-				loop={true}
-				autoplay={{
-					delay: 1500,
-					disableOnInteraction: false,
-					pauseOnMouseEnter: true,
-					reverseDirection: true,
-				}}
-				freeMode={{
-					enabled: true,
-					sticky: false,
-					momentum: true,
-				}}
-				className="h-screen overflow-hidden"
-			>
-				{[0].map((project, index) =>
-					<SwiperSlide key={index} className="grid grid-cols-1 lg:grid-cols-2 gap-4">
-						<Link href="/projects/[name]" as="/projects/pluctis" passHref>
-							<Project
-								name="Pluctis"
-								desc="Janvier 2021 - Mars 2021"
-							/>
-						</Link>
-						<Link href="/projects/[name]" as="/projects/pluctis" passHref>
-							<Project
-								name="Pluctis"
-								desc="Janvier 2021 - Mars 2021"
-								className="lg:mt-12"
-							/>
-						</Link>
-					</SwiperSlide>
-				)}
-			</Swiper>
+			<div className="grid grid-cols-1 lg:grid-cols-2 gap-8">
+				{projects.map((project, index) => (
+					<Link key={project.name} href="/projects/[name]" as={`/projects/${project.name}`} passHref>
+						<Project
+							name={project.label}
+							image={project.images[0]}
+							desc={project.desc}
+							className={cx(
+								index % 2 !== 0 && 'mt-20',
+							)}
+						/>
+					</Link>
+				))}
+			</div>
 		</Section>
 	);
-};
 
 export default Projects;
